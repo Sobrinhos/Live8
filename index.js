add732a054d126cd632f596f839fc3a6c0de0fe1
Gustavo Souza <gustavosouzacco@gmail.com>
2022-09-12 22:37:58 -0300

    Primeira refatoração: Função amountFor

--- index.js
+++ index.js
@@ -17,31 +17,11 @@ function statement(invoice, plays) {
 
   for(let perf of invoice.performances){
     const play = plays[perf.playId];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000
-        if (perf.audience > 30) {
-          thisAmount += 1000*(perf.audience - 30)
-        }
-      break;
-
-      case 'comedy':
-        thisAmount = 30000
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 *(perf.audience - 20)
-        }
-        
-        thisAmount += 300* perf.audience
-      break;
     
-      default:
-        throw new Error('unknown type: ${play.type}')
-    }
+    let thisAmount = amountFor(perf, play);
 
     //Soma créditos por volume
-    volumeCredits += Math.max(perf.audience, 0)
+    volumeCredits += Math.max(perf.audience - 30, 0)
     // soma um crédito extra para cada dez esoectadores de comédia
     if("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
 
@@ -54,4 +34,30 @@ function statement(invoice, plays) {
   result+= `You earned ${volumeCredits} credits \n`; 
 
   return result;
+}
+
+function amountFor(perf, play) { 
+  let thisAmount = 0;
+  switch (play.type) {
+    case 'tragedy':
+      thisAmount = 40000
+      if (perf.audience > 30) {
+        thisAmount += 1000*(perf.audience - 30)
+      }
+    break;
+
+    case 'comedy':
+      thisAmount = 30000
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 *(perf.audience - 20)
+      }
+      
+      thisAmount += 300* perf.audience
+    break;
+  
+    default:
+      throw new Error('unknown type: ${play.type}')
+  }
+
+  return thisAmount;
 }
