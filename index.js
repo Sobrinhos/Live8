4a6672e98cb2a4721de24c582ee4cc0d2fc2b24a
Gustavo Souza <gustavosouzacco@gmail.com>
2022-09-15 23:08:35 -0300

    Live 11

--- index.js
+++ index.js
@@ -1,72 +1,44 @@
 const invoices = require("./invoices.json");
-const plays = require("./plays.json");
+const createStatementData = require("./createStatementData");
 
 console.log(statement(invoices));
+console.log(htmlStatement(invoices));
 
 function statement(invoice) { 
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances
-  return renderPlainText(statementData);
+  return renderPlainText(createStatementData(invoice));
 }
 
 function renderPlainText(data) { 
   let result = `Statement for ${data.customer}\n`;
 
   for(let aPerformance of data.performances){    
-    result += ` ${playFor(aPerformance).name}: ${valueToBRL(amountFor(aPerformance)/100)} (${aPerformance.audience} seats) \n`
+    result += ` ${aPerformance.play.name}: ${valueToBRL(aPerformance.amount)} (${aPerformance.audience} seats) \n`
   }
 
-  result+= `Amount owed is ${valueToBRL(totalAmount()/100)} \n`; 
-  result+= `You earned ${totalVolumeCredits()} credits \n`; 
+  result+= `Amount owed is ${valueToBRL(data.totalAmount)} \n`; 
+  result+= `You earned ${data.totalVolumeCredits} credits \n`; 
 
   return result;
 }
 
-function amountFor(aPerformance) { 
-  let result = 0;
-  switch (playFor(aPerformance).type) {
-    case 'tragedy':
-      result = 40000
-      if (aPerformance.audience > 30) {
-        result += 1000*(aPerformance.audience - 30)
-      }
-    break;
+function htmlStatement(invoice) { 
+  return renderHtml(createStatementData(invoice));
+}
 
-    case 'comedy':
-      result = 30000
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 *(aPerformance.audience - 20)
-      }
-      
-      result += 300* aPerformance.audience
-    break;
-  
-    default:
-      throw new Error(`unknown type: ${playFor(aPerformance).type}`)
+function renderHtml(data) { 
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th>\n";
+  for(let aPerformance of data.performances){    
+    result += `<tr><td>${aPerformance.play.name}</td><td>${aPerformance.audience}</td><td>${valueToBRL(aPerformance.amount)}</td></tr>\n`
   }
+  result += "</table>\n";
+
+  result+= `<p>Amount owed is <em>${valueToBRL(data.totalAmount)}</em></p> \n`; 
+  result+= `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p> \n`; 
 
   return result;
-}
-
-function playFor(aPerformance) { 
-  return plays[aPerformance.playId];
-}
-
-function volumeCreditsFor(aPerformance) { 
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0)
-  if("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-
-  return result;
-}
-
-function totalVolumeCredits() { 
-  let volumeCredits = 0;
-  for(let aPerformance of invoices.performances){  
-    volumeCredits += volumeCreditsFor(aPerformance)
-  }
-  return volumeCredits;
 }
 
 function valueToBRL(value) { 
@@ -74,13 +46,5 @@ function valueToBRL(value) {
     style: "currency",
     currency: "BRL",
     minimumFractionDigits: 2
-  }).format(value);
-}
-
-function totalAmount() { 
-  let result = 0;
-  for(let aPerformance of invoices.performances){ 
-    result += amountFor(aPerformance);
-  } 
-  return result;
+  }).format(value/100);
 }
